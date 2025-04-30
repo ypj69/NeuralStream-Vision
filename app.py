5b9289af6243fa91d8deb13e13f67f7bf1c27ffc
ypj69 <1620804244@qq.com>
2025-04-30 15:14:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+# 添加中文字体支持
+plt.rcParams['font.sans-serif'] = ['DejaVu Sans']  # 使用 DejaVu Sans 字体
+plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 from genData import get_stock_data, preprocess_data, StockDataset, rsi, williams_r, emv, sma, obv, volume_change_rate, amount_change_rate, volume_ma, amount_ma, price_diff_features
 from StockFundamentals import StockFundamentals
 from new_mian import main
